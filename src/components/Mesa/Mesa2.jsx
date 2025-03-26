faf93533e5afcfc172faae24003665c39a73002c
drakhz <raul.junquera.abellan@cieep.com>
2025-03-26 15:43:44 +0100

    Correcion Reposicionamiento patas

--- src/components/Mesa/Mesa2.jsx
+++ src/components/Mesa/Mesa2.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, Box } from '@react-three/drei'
 import { useConfigurator } from '../../contexts/Configurator';
 import * as Three from 'three'
 import { useFrame } from '@react-three/fiber'
@@ -31,6 +31,8 @@ export function Mesa2(props) {
     }, [plankTexture]);
 
 
+
+
     useFrame((state, delta) => {
         const tableWidthScale = tableWidth / 100;
         const tableHeightScale = tableHeight / 100;
@@ -42,35 +44,44 @@ export function Mesa2(props) {
                 delta * 12
             );
             plank.current.material.map.repeat.set(tableDepthScale, tableWidthScale);
+
+            const geometry = plank.current.geometry;
+
+            // Crear un bounding box
+            const box = new Three.Box3().setFromObject(plank.current);
+            const size = new Three.Vector3();
+            box.getSize(size);
+
+            if (rightBackLeg.current && rightFrontLeg.current && leftBackLeg.current && leftFrontLeg.current) {
+                leftBackLeg.current.position.lerp(new Three.Vector3(-size.x/2 + (size.x*10/100), leftBackLeg.current.position.y, -size.z/2 +( size.z*10/100)), delta * 12);
+                rightBackLeg.current.position.lerp(new Three.Vector3(size.x/2- (size.x*10/100), leftBackLeg.current.position.y, -size.z/2 +( size.z*10/100)), delta * 12);
+                leftFrontLeg.current.position.lerp(new Three.Vector3(-size.x/2 + (size.x*10/100), leftBackLeg.current.position.y, size.z/2 -( size.z*10/100)), delta * 12);
+                rightFrontLeg.current.position.lerp(new Three.Vector3(size.x/2- (size.x*10/100), leftBackLeg.current.position.y, size.z/2 -( size.z*10/100)), delta * 12);
+    
+                if (connectorBack.current && connectorFront.current) {
+                    connectorBack.current.position.lerp(new Three.Vector3(-size.x/2 + (size.x*10/100), 1.318, 0), delta * 12);
+                    connectorFront.current.position.lerp(new Three.Vector3(size.x/2 - (size.x*10/100), 1.318, 0), delta * 12);
+    
+                    connectorBack.current.scale.lerp(new Three.Vector3(1, 1, size.z/2), delta * 12);
+                    connectorFront.current.scale.lerp(new Three.Vector3(1, 1, size.z/2), delta * 12);
+                }
+    
+                if (connectorBackUp.current && connectorFrontUp.current && connectorBackDown.current && connectorFrontDown.current) {
+                    connectorBackUp.current.position.lerp(new Three.Vector3(-size.x/2 + (size.x*10/100), 1.332, 0), delta * 12);
+                    connectorFrontUp.current.position.lerp(new Three.Vector3(size.x/2 - (size.x*10/100), 1.332, 0), delta * 12);
+                    connectorBackDown.current.position.lerp(new Three.Vector3(-size.x/2 + (size.x*10/100), -0.121, 0), delta * 12);
+                    connectorFrontDown.current.position.lerp(new Three.Vector3(size.x/2 - (size.x*10/100), -0.121, 0), delta * 12);
+    
+                    connectorBackUp.current.scale.lerp(new Three.Vector3(1, 1, size.z/2), delta * 12);
+                    connectorFrontUp.current.scale.lerp(new Three.Vector3(1, 1, size.z/2), delta * 12);
+                    connectorBackDown.current.scale.lerp(new Three.Vector3(1, 1, size.z/2), delta * 12);
+                    connectorFrontDown.current.scale.lerp(new Three.Vector3(1, 1, size.z/2), delta * 12);
+                }
+            }
         }
 
-        if (rightBackLeg.current && rightFrontLeg.current && leftBackLeg.current && leftFrontLeg.current) {
-            leftBackLeg.current.position.lerp(new Three.Vector3(-tableWidthScale, 0.596, -tableDepthScale/1.225), delta * 12);
-            rightBackLeg.current.position.lerp(new Three.Vector3(tableWidthScale, 0.596, -tableDepthScale/1.225), delta * 12);
-            leftFrontLeg.current.position.lerp(new Three.Vector3(-tableWidthScale, 0.596, tableDepthScale/1.225), delta * 12);
-            rightFrontLeg.current.position.lerp(new Three.Vector3(tableWidthScale, 0.596, tableDepthScale/1.225), delta * 12);
 
-            if (connectorBack.current && connectorFront.current){
-                connectorBack.current.position.lerp(new Three.Vector3(-tableWidthScale,1.318,0),delta*12);
-                connectorFront.current.position.lerp(new Three.Vector3(tableWidthScale,1.318,0),delta*12);
 
-                connectorBack.current.scale.lerp(new Three.Vector3(1,1,tableDepthScale),delta*12);
-                connectorFront.current.scale.lerp(new Three.Vector3(1,1,tableDepthScale),delta*12);
-            }
-
-            if (connectorBackUp.current && connectorFrontUp.current && connectorBackDown.current && connectorFrontDown.current){
-                connectorBackUp.current.position.lerp(new Three.Vector3(-tableWidthScale,1.332,0),delta*12);
-                connectorFrontUp.current.position.lerp(new Three.Vector3(tableWidthScale,1.332,0),delta*12);
-                connectorBackDown.current.position.lerp(new Three.Vector3(-tableWidthScale,-0.121,0),delta*12);
-                connectorFrontDown.current.position.lerp(new Three.Vector3(tableWidthScale,-0.121,0),delta*12);
-
-                connectorBackUp.current.scale.lerp(new Three.Vector3(1,1,tableDepthScale),delta*12);
-                connectorFrontUp.current.scale.lerp(new Three.Vector3(1,1,tableDepthScale),delta*12);
-                connectorBackDown.current.scale.lerp(new Three.Vector3(1,1,tableDepthScale),delta*12);
-                connectorFrontDown.current.scale.lerp(new Three.Vector3(1,1,tableDepthScale),delta*12);
-            }
-        }
-        console.log(materials.Madera);
     });
 
     const plank = useRef();
@@ -90,6 +101,7 @@ export function Mesa2(props) {
 
 
     return (
+
         <group {...props} dispose={null}>
             <group>
                 <mesh castShadow geometry={nodes.pCube1.geometry} material={materials.Madera} position={[0, 1.475, 0]} ref={plank} />
@@ -104,25 +116,25 @@ export function Mesa2(props) {
                 )}
                 {legs === 1 && (
                     <>
-                        <mesh castShadow geometry={nodes.polySurface18.geometry} material={materials.LightMetal} position={[-1.326, 1.318, -0.634]} ref={connectorBack}/>    
-                        <mesh castShadow geometry={nodes.polySurface20.geometry} material={materials.LightMetal} position={[-1.326, 0.617, 0.246]} ref={leftFrontLeg}/>
-                        <mesh castShadow geometry={nodes.polySurface21.geometry} material={materials.LightMetal} position={[-1.326, 0.618, -1.514]} ref={leftBackLeg}/>
-                        
-                        <mesh castShadow geometry={nodes.polySurface22.geometry} material={materials.LightMetal} position={[1.326, 1.318, -0.634]} ref={connectorFront}/>
-                        <mesh castShadow geometry={nodes.polySurface24.geometry} material={materials.LightMetal} position={[1.326, 0.617, 0.246]} ref={rightFrontLeg}/>
-                        <mesh castShadow geometry={nodes.polySurface25.geometry} material={materials.LightMetal} position={[1.326, 0.618, -1.514]} ref={rightBackLeg}/>
+                        <mesh castShadow geometry={nodes.polySurface18.geometry} material={materials.LightMetal} position={[-1.326, 1.318, -0.634]} ref={connectorBack} />
+                        <mesh castShadow geometry={nodes.polySurface20.geometry} material={materials.LightMetal} position={[-1.326, 0.617, 0.246]} ref={leftFrontLeg} />
+                        <mesh castShadow geometry={nodes.polySurface21.geometry} material={materials.LightMetal} position={[-1.326, 0.618, -1.514]} ref={leftBackLeg} />
+
+                        <mesh castShadow geometry={nodes.polySurface22.geometry} material={materials.LightMetal} position={[1.326, 1.318, -0.634]} ref={connectorFront} />
+                        <mesh castShadow geometry={nodes.polySurface24.geometry} material={materials.LightMetal} position={[1.326, 0.617, 0.246]} ref={rightFrontLeg} />
+                        <mesh castShadow geometry={nodes.polySurface25.geometry} material={materials.LightMetal} position={[1.326, 0.618, -1.514]} ref={rightBackLeg} />
                     </>
                 )}
                 {legs === 2 && (
                     <>
-                        <mesh castShadow geometry={nodes.polySurface27001.geometry} material={materials.LightMetal} position={[1.326, -0.121, -0.636]} ref={connectorFrontDown}/>
-                        <mesh castShadow geometry={nodes.polySurface28001.geometry} material={materials.LightMetal} position={[1.326, 1.332, -0.636]} ref={connectorFrontUp}/>
-                        <mesh castShadow geometry={nodes.polySurface30001.geometry} material={materials.LightMetal} position={[1.326, 0.606, -1.518]} ref={rightBackLeg}/>
-                        <mesh castShadow geometry={nodes.polySurface31001.geometry} material={materials.LightMetal} position={[1.326, 0.606, 0.246]} ref={rightFrontLeg}/>
-                        <mesh castShadow geometry={nodes.polySurface27.geometry} material={materials.LightMetal} position={[-1.326, -0.121, -0.636]} ref={connectorBackDown}/>
-                        <mesh castShadow geometry={nodes.polySurface28.geometry} material={materials.LightMetal} position={[-1.326, 1.332, -0.636]} ref={connectorBackUp}/>
-                        <mesh castShadow geometry={nodes.polySurface30.geometry} material={materials.LightMetal} position={[-1.326, 0.606, -1.518]} ref={leftBackLeg}/>
-                        <mesh castShadow geometry={nodes.polySurface31.geometry} material={materials.LightMetal} position={[-1.326, 0.606, 0.246]} ref={leftFrontLeg}/>
+                        <mesh castShadow geometry={nodes.polySurface27001.geometry} material={materials.LightMetal} position={[1.326, -0.121, -0.636]} ref={connectorFrontDown} />
+                        <mesh castShadow geometry={nodes.polySurface28001.geometry} material={materials.LightMetal} position={[1.326, 1.332, -0.636]} ref={connectorFrontUp} />
+                        <mesh castShadow geometry={nodes.polySurface30001.geometry} material={materials.LightMetal} position={[1.326, 0.606, -1.518]} ref={rightBackLeg} />
+                        <mesh castShadow geometry={nodes.polySurface31001.geometry} material={materials.LightMetal} position={[1.326, 0.606, 0.246]} ref={rightFrontLeg} />
+                        <mesh castShadow geometry={nodes.polySurface27.geometry} material={materials.LightMetal} position={[-1.326, -0.121, -0.636]} ref={connectorBackDown} />
+                        <mesh castShadow geometry={nodes.polySurface28.geometry} material={materials.LightMetal} position={[-1.326, 1.332, -0.636]} ref={connectorBackUp} />
+                        <mesh castShadow geometry={nodes.polySurface30.geometry} material={materials.LightMetal} position={[-1.326, 0.606, -1.518]} ref={leftBackLeg} />
+                        <mesh castShadow geometry={nodes.polySurface31.geometry} material={materials.LightMetal} position={[-1.326, 0.606, 0.246]} ref={leftFrontLeg} />
                     </>
                 )}
                 {legs === 3 && (
