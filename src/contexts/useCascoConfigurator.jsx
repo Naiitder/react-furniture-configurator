1e68134cb31002520985d505657c3298123af130
Naiitder <naiitder@gmail.com>
2025-04-02 15:08:04 +0200

    sas

--- src/contexts/useCascoConfigurator.jsx
+++ src/contexts/useCascoConfigurator.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext } from "react";
+import Pata from "../components/Casco/Pata.js";
 
 const CascoConfiguratorContext = createContext();
 
@@ -7,6 +8,14 @@ export const CascoConfiguratorProvider = ({children}) => {
     const [height, setHeight] = useState(100);
     const [depth, setDepth] = useState(100);
     const [texture, setTexture] = useState("./textures/oak.jpg");
+    const [esquinaXTriangulada, setEsquinaXTriangulada] = useState(false);
+    const [esquinaZTriangulada, setEsquinaZTriangulada] = useState(false);
+    const [espesor, setEspesor] = useState(0.1);
+    const [sueloDentro, setSueloDentro] = useState(false);
+    const [techoDentro, setTechoDentro] = useState(false);
+    const [traseroDentro, setTraseroDentro] = useState(false);
+    const [offsetTrasero, setOffsetTrasero] = useState(0);
+    const [pataHeight, setPataHeight] = useState(0);
 
     return <CascoConfiguratorContext.Provider value={{
         width,
@@ -16,7 +25,23 @@ export const CascoConfiguratorProvider = ({children}) => {
         depth,
         setDepth,
         texture,
-        setTexture
+        setTexture,
+        esquinaXTriangulada,
+        setEsquinaXTriangulada,
+        esquinaZTriangulada,
+        setEsquinaZTriangulada,
+        espesor,
+        setEspesor,
+        sueloDentro,
+        setSueloDentro,
+        techoDentro,
+        setTechoDentro,
+        traseroDentro,
+        setTraseroDentro,
+        offsetTrasero,
+        setOffsetTrasero,
+        pataHeight,
+        setPataHeight,
     }}>
         {children}
     </CascoConfiguratorContext.Provider>
