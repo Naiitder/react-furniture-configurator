7db65510374c5e542c58697efe152f206f55adc4
Naiitder <naiitder@gmail.com>
2025-04-03 17:19:54 +0200

    PatasJamon

--- src/components/Casco/Pata.tsx
+++ src/components/Casco/Pata.tsx
@@ -9,9 +9,15 @@ type PataProps = {
 
 const Pata: React.FC<PataProps> = ({ position = [0, 0, 0], height, color = "#8B4513" }) => {
     return (
-        <group position={[position[0], position[1] + height / 2, position[2]]}>
-            <mesh>
-                <cylinderGeometry args={[0.1, 0.1, height, 16]} />
+        <group>
+            <group position={[position[0], position[1] + height / 2, position[2]]}>
+                <mesh>
+                    <cylinderGeometry args={[0.075, 0.075, height, 12]} />
+                    <meshStandardMaterial color={color} />
+                </mesh>
+            </group>
+            <mesh position={[position[0], position[1] + 0.025, position[2] ]}>
+                <cylinderGeometry args={[0.1, 0.1, .05, 12]} />
                 <meshStandardMaterial color={color} />
             </mesh>
         </group>
