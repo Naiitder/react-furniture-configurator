785ebf6f053e3766c1120df84f7c7e1b70b3c791
Naiitder <naiitder@gmail.com>
2025-04-03 09:38:38 +0200

    Refactor

--- src/contexts/AppProviders.jsx
+++ src/contexts/AppProviders.jsx
@@ -1,19 +1,13 @@
 import {RoomConfiguratorProvider} from "./RoomConfigurator.jsx";
-import {ConfiguratorProvider} from "./Configurator.jsx";
-import {ArmarioConfiguratorProvider} from "./ArmarioConfigurator.jsx";
 import {CascoConfiguratorProvider} from "./useCascoConfigurator.jsx";
 
 export const AppProviders = ({...props}) => {
     return (
         <>
             <CascoConfiguratorProvider>
-            <ConfiguratorProvider>
                 <RoomConfiguratorProvider>
-                    <ArmarioConfiguratorProvider>
                         {props.children}
-                    </ArmarioConfiguratorProvider>
                 </RoomConfiguratorProvider>
-            </ConfiguratorProvider>
             </CascoConfiguratorProvider>
         </>
     )
