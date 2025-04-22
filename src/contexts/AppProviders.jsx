c1df721dabcf0ff180b11f5750a12f40222fb772
Adrián Primo <adrianprimob@gmail.com>
2025-04-22 09:57:31 +0200

    :sparkles: Empezado historial de acciones

--- src/contexts/AppProviders.jsx
+++ src/contexts/AppProviders.jsx
@@ -3,19 +3,22 @@ import {SelectedItemProvider} from "./SelectedItemProvider.jsx";
 import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import {SelectedPieceProvider} from "./SelectedPieceProvider.jsx";
+import {ActionHistoryProvider} from "./ActionHistoryProvider.jsx";
 
 export const AppProviders = ({...props}) => {
     return (
         <>
-            <SelectedItemProvider>
-                <SelectedPieceProvider>
-                <DndProvider backend={HTML5Backend}>
-                    <RoomConfiguratorProvider>
-                            {props.children}
-                    </RoomConfiguratorProvider>
-                </DndProvider>
-                </SelectedPieceProvider>
-            </SelectedItemProvider>
+            <ActionHistoryProvider>
+                <SelectedItemProvider>
+                    <SelectedPieceProvider>
+                        <DndProvider backend={HTML5Backend}>
+                            <RoomConfiguratorProvider>
+                                {props.children}
+                            </RoomConfiguratorProvider>
+                        </DndProvider>
+                    </SelectedPieceProvider>
+                </SelectedItemProvider>
+            </ActionHistoryProvider>
         </>
     )
 }
