f1fb4171cc5f851df190004484d36265bda67510
Raul <naiitder@gmail.com>
2025-09-08 09:26:02 +0200

    ReinicioRaycast

--- src/utils/interseccionRaycaster.tsx
+++ src/utils/interseccionRaycaster.tsx
@@ -8,16 +8,14 @@ const InterseccionRaycaster = ({ x, y, interseccion}: { x: number; y: number; in
     const { scene } = useThree();
 
     useEffect(() => {
-        console.log("empezar");
-
-        const geometry = new THREE.BoxGeometry(.1, .1, .1);
+        const geometry = new THREE.BoxGeometry(.1, .1, .1   );
         const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
         const mesh = new THREE.Mesh(geometry, material);
         mesh.position.set(x, y, 0.1);
 
         scene.add(mesh);
 
-        console.log("llegar");
+
 
         return () => {
             scene.remove(mesh); // Limpieza
